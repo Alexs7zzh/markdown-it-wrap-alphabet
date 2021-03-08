1e0e706a3cbe42953a585ddefbc2bd7b0940d398
Alex <alex.zzh.s7@gmail.com>
2021-03-09 02:25:00 +0900

    Add wrapAll option

--- index.js
+++ index.js
@@ -8,24 +8,16 @@ const isEn = token =>
 const isCJK = token =>
   token && token.type === 'text' && token.meta && !token.meta.isEn
 
-const en_open = (before = false, after = false) => {
-  let open = new Token('en_open', 'span', 1)
-  open.meta = {
-    before: before,
-    after: after,
-  }
-  return open
-}
-const en_close = () => new Token('en_close', 'span', -1)
-
 const getEnglish = text => {
   return [...text.matchAll(/([\w,.;'"’‘”“ ()\-–—…+!?&/<>*[\]:@#=]+)/g)].filter(m => /[a-zA-Z]/.test(m[0]))
 }
 
 const hasEnglish = text => {
   const matches = getEnglish(text)
-  return /\p{Unified_Ideograph}/u.test(text) && matches.length !== 0
+  return matches.length !== 0
 }
+
+const hasCJK = text => /\p{Unified_Ideograph}/u.test(text)
 
 const fragmentize = text => {
   const matches = getEnglish(text)
@@ -77,140 +69,141 @@ const arrayReplaceRange = (src, posStart, posEnd, newElements) => {
 //   })
 // }
 
-const tokenize = state => {
-  for (let idx = state.tokens.length - 1; idx >= 0; idx--) {
-    if (state.tokens[idx].type !== 'inline') continue
-
-    if (!hasEnglish(state.tokens[idx].content)) continue
-
-    let inlineTokens = state.tokens[idx].children
-    
-    // break text into English and CJK
-    for (let i = inlineTokens.length - 1; i >= 0; i--) {
-      if (inlineTokens[i].type !== 'text') continue
-      if (!inlineTokens[i].content || inlineTokens[i].content === '') continue
-      let fragments = fragmentize(inlineTokens[i].content)
-      fragments = fragments.map(f => {
-        let tmp = new Token('text', '', 0)
-        tmp.content = f.text
-        tmp.meta = { isEn: f.isEn }
-        return tmp
-      })
-      state.tokens[idx].children = inlineTokens = arrayReplaceAt(inlineTokens, i, fragments)
-    }
-
-    if (inlineTokens.length === 0) continue
-
-    let nesting = 0, lastPos = -1
-    const terminators = ['en_close', 'footnote_ref', 'hardbreak']
-
-    for (let i = 0; i < inlineTokens.length; i++) {
-      nesting += inlineTokens[i].nesting
-      if (isCJK(inlineTokens[i])) lastPos = i
-
-      // only stop at English text
-      if (!isEn(inlineTokens[i])) continue
-
-      // expand as mush as possible, does not include right
-      let left = i, right = i, leftNesting = nesting, rightNesting = nesting, tagsToClose = []
-      while (left >= 0 && !terminators.includes(inlineTokens[left].type) && !isCJK(inlineTokens[left]) && inlineTokens[right].nesting !== -1) {
-        if (inlineTokens[left].nesting === 1) tagsToClose.push(inlineTokens[left].type.replace('_open', ''))
-        leftNesting -= inlineTokens[left--].nesting
-      }
-      while (right < inlineTokens.length && !terminators.includes(inlineTokens[right].type) && !isCJK(inlineTokens[right])) {
-        if (inlineTokens[right].nesting === 1) tagsToClose.push(inlineTokens[right].type.replace('_open', ''))
-        if (inlineTokens[right].nesting === -1) 
-          if (tagsToClose.includes(inlineTokens[right].type.replace('_close', ''))) {
-            const index = tagsToClose.indexOf(inlineTokens[right].type.replace('_close', ''))
-            tagsToClose.splice(index, 1)
-          } else 
-            break
-        
-        rightNesting += inlineTokens[right++].nesting
-      }
-      left++
-
-      // adjust left or right pointers
-      if (leftNesting !== rightNesting) 
-        if (Math.abs(leftNesting - nesting) > Math.abs(rightNesting - nesting)) 
-          while (leftNesting !== rightNesting && left < inlineTokens.length) leftNesting += inlineTokens[left++].nesting
-        else {
-          if (right === inlineTokens.length) right--
-          while (!(leftNesting === rightNesting && tagsToClose.length === 0) && right >= 0) {
-            rightNesting -= inlineTokens[right].nesting
-            if (inlineTokens[right].nesting === 1) {
-              const index = tagsToClose.indexOf(inlineTokens[right].type.replace('_open', ''))
-              tagsToClose.splice(index, 1)
-            }
-            if (inlineTokens[right].nesting === -1) tagsToClose.push(inlineTokens[right].type.replace('_close', ''))
-            right--
-          }
-          right++
-        }
-      
-      
-      let before = false, after = false, p
-      // trim space and add classes
-      if ((!inlineTokens[left - 1] || !terminators.includes(inlineTokens[left - 1].type)) && isCJK(inlineTokens[lastPos])) {
-        // Trim start of current token array
-        p = left
-        while (p < right && inlineTokens[p] && inlineTokens[p].type !== 'text') p++
-        if (inlineTokens[p]) inlineTokens[p].content = inlineTokens[p].content.trimStart()
-
-        if (inlineTokens[lastPos]) {
-          inlineTokens[lastPos].content = inlineTokens[lastPos].content.trimEnd()
-          const content = inlineTokens[lastPos].content
-          if(!chinesePunc.includes(content[content.length - 1])) before = true
-        }
-      }
-
-      p = right
-      while (p < inlineTokens.length && inlineTokens[p] && inlineTokens[p].type !== 'text') p++
-      if (inlineTokens[p] && isCJK(inlineTokens[p])) {
-        inlineTokens[p].content = inlineTokens[p].content.trimStart()
-        const content = inlineTokens[p].content
-        if(!chinesePunc.includes(content[0])) after = true
-
-        p = right - 1
-        while (p >= left && inlineTokens[p] && inlineTokens[p].type !== 'text') p--
-        if (inlineTokens[p]) inlineTokens[p].content = inlineTokens[p].content.trimEnd()
-      }
-
-      // wrapping
-      state.tokens[idx].children = inlineTokens = arrayReplaceRange(inlineTokens, left, right, [
-        en_open(before, after),
-        ...inlineTokens.slice(left, right),
-        en_close()
-      ])
-
-      lastPos = i
-      
-      i = i + (right - left)
-      if (i < inlineTokens.length) nesting = rightNesting
-
-    }
-
-  }
-}
-
 module.exports = (md, opts) => {
   const defaultOptions = {
     before: 'before',
     after: 'after',
-    lang: ''
+    lang: '',
+    wrapAll: false
   }
   opts = Object.assign({}, defaultOptions, opts)
 
-  const render_en_open = (tokens, idx) => {
-    const meta = tokens[idx].meta
-    let classes = []
-    if (meta.before) classes.push(opts.before)
-    if (meta.after)  classes.push(opts.after)
-    return `<span ${opts.lang === '' ? '' : `lang='${opts.lang}' `}class='${classes.join(' ')}'>`
+  const en_open = (before = false, after = false) => {
+    let open = new Token('en_open', 'span', 1)
+    if (before) open.attrJoin('class', opts.before)
+    if (after)  open.attrJoin('class', opts.after)
+    if (opts.lang !== '') open.attrPush(['lang', opts.lang])
+    return open
   }
-  const render_en_close = () => '</span>'
+  const en_close = () => new Token('en_close', 'span', -1)
 
-  md.renderer.rules.en_open = render_en_open
-  md.renderer.rules.en_close = render_en_close
+  const tokenize = state => {
+    if (!hasCJK(state.src)) return false
+    for (let idx = state.tokens.length - 1; idx >= 0; idx--) {
+      if (state.tokens[idx].type !== 'inline') continue
+
+      if (!hasEnglish(state.tokens[idx].content)) continue
+      if (!opts.wrapAll && !hasCJK(state.tokens[idx].content)) continue
+
+      let inlineTokens = state.tokens[idx].children
+      
+      // break text into English and CJK
+      for (let i = inlineTokens.length - 1; i >= 0; i--) {
+        if (inlineTokens[i].type !== 'text') continue
+        if (!inlineTokens[i].content || inlineTokens[i].content === '') continue
+        let fragments = fragmentize(inlineTokens[i].content)
+        fragments = fragments.map(f => {
+          let tmp = new Token('text', '', 0)
+          tmp.content = f.text
+          tmp.meta = { isEn: f.isEn }
+          return tmp
+        })
+        state.tokens[idx].children = inlineTokens = arrayReplaceAt(inlineTokens, i, fragments)
+      }
+
+      if (inlineTokens.length === 0) continue
+
+      let nesting = 0, lastPos = -1
+      const terminators = ['en_close', 'footnote_ref', 'hardbreak']
+
+      for (let i = 0; i < inlineTokens.length; i++) {
+        nesting += inlineTokens[i].nesting
+        if (isCJK(inlineTokens[i])) lastPos = i
+
+        // only stop at English text
+        if (!isEn(inlineTokens[i])) continue
+
+        // expand as mush as possible, does not include right
+        let left = i, right = i, leftNesting = nesting, rightNesting = nesting, tagsToClose = []
+        while (left >= 0 && !terminators.includes(inlineTokens[left].type) && !isCJK(inlineTokens[left]) && inlineTokens[right].nesting !== -1) {
+          if (inlineTokens[left].nesting === 1) tagsToClose.push(inlineTokens[left].type.replace('_open', ''))
+          leftNesting -= inlineTokens[left--].nesting
+        }
+        while (right < inlineTokens.length && !terminators.includes(inlineTokens[right].type) && !isCJK(inlineTokens[right])) {
+          if (inlineTokens[right].nesting === 1) tagsToClose.push(inlineTokens[right].type.replace('_open', ''))
+          if (inlineTokens[right].nesting === -1) 
+            if (tagsToClose.includes(inlineTokens[right].type.replace('_close', ''))) {
+              const index = tagsToClose.indexOf(inlineTokens[right].type.replace('_close', ''))
+              tagsToClose.splice(index, 1)
+            } else 
+              break
+          
+          rightNesting += inlineTokens[right++].nesting
+        }
+        left++
+
+        // adjust left or right pointers
+        if (leftNesting !== rightNesting) 
+          if (Math.abs(leftNesting - nesting) > Math.abs(rightNesting - nesting)) 
+            while (leftNesting !== rightNesting && left < inlineTokens.length) leftNesting += inlineTokens[left++].nesting
+          else {
+            if (right === inlineTokens.length) right--
+            while (!(leftNesting === rightNesting && tagsToClose.length === 0) && right >= 0) {
+              rightNesting -= inlineTokens[right].nesting
+              if (inlineTokens[right].nesting === 1) {
+                const index = tagsToClose.indexOf(inlineTokens[right].type.replace('_open', ''))
+                tagsToClose.splice(index, 1)
+              }
+              if (inlineTokens[right].nesting === -1) tagsToClose.push(inlineTokens[right].type.replace('_close', ''))
+              right--
+            }
+            right++
+          }
+        
+        
+        let before = false, after = false, p
+        // trim space and add classes
+        if ((!inlineTokens[left - 1] || !terminators.includes(inlineTokens[left - 1].type)) && isCJK(inlineTokens[lastPos])) {
+          // Trim start of current token array
+          p = left
+          while (p < right && inlineTokens[p] && inlineTokens[p].type !== 'text') p++
+          if (inlineTokens[p]) inlineTokens[p].content = inlineTokens[p].content.trimStart()
+
+          if (inlineTokens[lastPos]) {
+            inlineTokens[lastPos].content = inlineTokens[lastPos].content.trimEnd()
+            const content = inlineTokens[lastPos].content
+            if(!chinesePunc.includes(content[content.length - 1])) before = true
+          }
+        }
+
+        p = right
+        while (p < inlineTokens.length && inlineTokens[p] && inlineTokens[p].type !== 'text') p++
+        if (inlineTokens[p] && isCJK(inlineTokens[p])) {
+          inlineTokens[p].content = inlineTokens[p].content.trimStart()
+          const content = inlineTokens[p].content
+          if(!chinesePunc.includes(content[0])) after = true
+
+          p = right - 1
+          while (p >= left && inlineTokens[p] && inlineTokens[p].type !== 'text') p--
+          if (inlineTokens[p]) inlineTokens[p].content = inlineTokens[p].content.trimEnd()
+        }
+
+        // wrapping
+        state.tokens[idx].children = inlineTokens = arrayReplaceRange(inlineTokens, left, right, [
+          en_open(before, after),
+          ...inlineTokens.slice(left, right),
+          en_close()
+        ])
+
+        lastPos = i
+        
+        i = i + (right - left)
+        if (i < inlineTokens.length) nesting = rightNesting
+
+      }
+
+    }
+  }
+
   md.core.ruler.push('wrapEnglish', tokenize)
 }
